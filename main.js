950567c8f8e35e0a9845d07a414555aeaf344f0c
AOTOMCS <142975464+AOTOMCS@users.noreply.github.com>
2023-11-26 15:46:42 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import VirtualJoystick from "./src/virtual-joystick.js";
 
-const gameContainer = document.querySelector('main');
-document.body.appendChild(gameContainer);
+const stickContainer = document.querySelector('main');
+document.body.appendChild(stickContainer);
 
 const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 if (isMobile) {
@@ -9,7 +9,7 @@ if (isMobile) {
     joystickContainer.style.position = 'absolute';
     joystickContainer.style.bottom = '20px';
     joystickContainer.style.left = '20px';
-    gameContainer.appendChild(joystickContainer);
+    stickContainer.appendChild(joystickContainer);
     const joystick = new VirtualJoystick(joystickContainer, {
     width: 150,
     height: 150,
@@ -24,22 +24,7 @@ if (isMobile) {
     },
     });
 }
+else {
+    document.querySelector('main').innerHTML = "PC";
+}
 
-const joystickContainer = document.createElement('div');
-joystickContainer.style.position = 'absolute';
-joystickContainer.style.bottom = '20px';
-joystickContainer.style.left = '20px';
-gameContainer.appendChild(joystickContainer);
-const joystick = new VirtualJoystick(joystickContainer, {
-    width: 150,
-    height: 150,
-    color: 'gray',
-    handleColor: 'black',
-    handleRadius: 38,
-    onChange: function(delta) {
-        if (joystick.delta.x > 100) {
-            document.querySelector("#test").style.display = "none"
-        }
-    console.log(delta);
-    },
-    });
